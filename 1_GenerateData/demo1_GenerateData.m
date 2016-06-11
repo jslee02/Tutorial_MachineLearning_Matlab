beedefb1655a42327222be1b899a6a43bbff3656
Terry Taewoong Um <terry.t.um@gmail.com>
2016-06-11 07:37:31 -0400

    Add files via upload

---
+++ 1_GenerateData/demo1_GenerateData.m
@@ -0,0 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% [Author] Terry Taewoong Um (terry.t.um@gmail.com) %
+% Adaptive Systems Lab., University of Waterloo     %
+% https://www.facebook.com/terryum.io/              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Please leave the author information if you reuse the codes.
+
+% [x_train, y_train, x_test, y_test] = GenerateData(opt, bShow, nData, sigma, test_ratio)
+
+% opt : {gaussian(defalut), half_circle, full_circle, spiral}
+% bShow : {0: Don't plot, 1(defalut): plot}
+% nData : number of data (defalut: 1000)
+% sigma : noise standard deviation (default: 1)
+% test_ratio : ratio of test data (default: 0.2)
+
+close all;  clearvars;
+
+GenerateData();
+GenerateData('half_circle');
+GenerateData('full_circle', true, 800);
+GenerateData('spiral', true, 1200, 1.2, 0.4);
